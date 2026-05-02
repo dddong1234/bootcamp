8c1aa6e4b84746d9b8cd5ad34b27124e76e75193
SDH <sdh080200@gmail.com>
2026-05-02 20:22:37 +0900

    added machine_learning_course

--- 개인/my_pandas.py
+++ 개인/my_pandas.py
@@ -1,4 +1,5 @@
 import pandas as pd
-import sys
-print(sys.executable)
-print(pd.__version__)
+
+pima_columns = ['pregnancies', 'glucose', 'blood_pressure', 'skin_thickness', 'insulin', 'bmi', 'diabetes_pedigree_function', 'age', 'outcome']
+pima_data_url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv'
+
